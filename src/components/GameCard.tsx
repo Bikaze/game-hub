d17670cdf2c14533c414d94f3164659c5e8a6a15
Bikaze <clmntmugisha@gmail.com>
2024-09-20 01:59:16 +0200

    Refactor- move entiy interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
@@ -22,7 +22,8 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize="2xl">
-          <Link to={'/games/' + game.slug}>{game.name}</Link> <Emoji rating={game.rating_top} />{" "}
+          <Link to={"/games/" + game.slug}>{game.name}</Link>{" "}
+          <Emoji rating={game.rating_top} />{" "}
         </Heading>
       </CardBody>
     </Card>
