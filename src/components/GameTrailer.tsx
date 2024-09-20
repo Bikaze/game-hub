19d190c15b99615f4ab8424e23361892ce5c5b19
Bikaze <clmntmugisha@gmail.com>
2024-09-20 12:24:46 +0200

    Build Game Screenshots

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -1,3 +1,4 @@
+import { Spinner } from "@chakra-ui/react";
 import useTrailers from "../hooks/useTrailers";
 
 interface Props {
@@ -7,14 +8,14 @@ interface Props {
 const GameTrailer = ({ gameId }: Props) => {
   const { data, isLoading, error } = useTrailers(gameId);
 
-  if (isLoading) return null;
+  if (isLoading) return <Spinner/>;
 
   if (error) throw error;
 
   const first = data?.results[0];
 
   return first ? (
-    <video src={first.data.max} poster={first.preview} controls />
+    <video src={first.data[480]} poster={first.preview} controls />
   ) : null;
 };
 
